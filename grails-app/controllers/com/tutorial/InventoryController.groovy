3abdd357b05fb9d1fd2c9137d7a9bc96cca9ba85
Vinicius Cechin <vinicechin@gmail.com>
2018-05-21 16:27:36 -0300

    Added invetory controller

---
+++ grails-app/controllers/com/tutorial/InventoryController.groovy
@@ -0,0 +1,12 @@
+package com.tutorial
+
+class InventoryController {
+
+    def index() {
+      render "Hre is a list of everything"
+    }
+
+    def remove() {
+      render "You have one less than before"
+    }
+}
